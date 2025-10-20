2e1d396a0bf9bcfd262cb3b5886a2bac0b5ca7e6
george <george.moore@cyted.ai>
2025-10-20 17:37:46 +0100

    fix indentation in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,19 +1,19 @@
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Kubernetes (K8s) Wiki',
-  url: 'http://localhost',
-  baseUrl: '/',
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          routeBasePath: '/',
-        },
-        blog: false,
-      },
+    title: 'Kubernetes (K8s) Wiki',
+    url: 'http://localhost',
+    baseUrl: '/',
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    routeBasePath: '/',
+                },
+                blog: false,
+            },
+        ],
     ],
-  ],
 };
 
 export default config;
