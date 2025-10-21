de12008d264954c46bdc3579df4fa8c181457f66
george <george.moore@cyted.ai>
2025-10-21 19:27:25 +0100

    tweak the redirect logic in index.js

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-    return <Redirect to="/kubernetes" />;
+    const target = useBaseUrl('/kubernetes');
+    return <Redirect to={target} />;
 }
